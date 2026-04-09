ae850d085a216ca3e159338fa97015a704baf523
Xhuvo1m <saxhuvo@gmail.com>
2026-04-09 17:33:38 +0600

    Update main.py

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ def is_user_in_channel(user_id):
 
 
 def call_api(region, uid):
-    url = f"https://sir-xhuvo-codex-like.onrender.com/like?uid={uid}&server_name={region}"
+    url = f"https://sir-xhuvo-codex-like.api/like?uid={uid}&server_name={region}"
     try:
         response = requests.get(url, timeout=20)
         if response.status_code != 200:
